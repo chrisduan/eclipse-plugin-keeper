c8171ade1b3e796eed7a10cbc407d98d4861498f
ekesdua <kesong.duan@ericsson.com>
2018-08-27 09:23:18 +0800

    add timezones

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockView.java
@@ -1,20 +1,65 @@
 package com.packtpub.e4.clock.ui.views;
 
 
+import java.util.TimeZone;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
 
 public class ClockView extends ViewPart {
 
+	private Combo timezones;
+	
 	public void createPartControl(Composite parent) {
 		
-		final ClockWidget clock = new ClockWidget(parent,SWT.NONE);
+		RowLayout layout = new RowLayout(SWT.HORIZONTAL);
+		parent.setLayout(layout);
+
+		final ClockWidget clock = new ClockWidget(parent, SWT.NONE, new RGB(
+				255, 0, 0));
+		final ClockWidget clock2 = new ClockWidget(parent, SWT.NONE, new RGB(0,
+				255, 0));
+		final ClockWidget clock3 = new ClockWidget(parent, SWT.NONE, new RGB(0,
+				0, 255));
 		
+		
+		String[] ids = TimeZone.getAvailableIDs();
+		timezones = new Combo(parent, SWT.SIMPLE);
+		timezones.setVisibleItemCount(5);
+		for (int i = 0; i < ids.length; i++) {
+			timezones.add(ids[i]);
+		}
+		
+		timezones.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent e) {
+				String z = timezones.getText();
+				TimeZone tz = z == null ? null : TimeZone.getTimeZone(z);
+				TimeZone dt = TimeZone.getDefault();
+				int offset = tz == null ? 0 : (tz.getOffset(System
+						.currentTimeMillis()) - dt.getOffset(System
+						.currentTimeMillis())) / 3600000;
+				clock3.setOffset(offset);
+				clock3.redraw();
+			}
+
+			public void widgetDefaultSelected(SelectionEvent e) {
+				clock3.setOffset(0);
+				clock3.redraw();
+			}
+		});
 	}
 
 	
 	public void setFocus() {
+		
+		timezones.setFocus();
 	}
+
 }
--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockWidget.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/ClockWidget.java
@@ -3,22 +3,41 @@ package com.packtpub.e4.clock.ui.views;
 import java.util.Date;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 public class ClockWidget extends Canvas{
 
-	public ClockWidget(Composite parent, int style) {
+	private final Color color;
+	
+	private int offset;
+	
+	public void setOffset(int offset) {
+	this.offset = offset;
+	}
+	
+	public ClockWidget(Composite parent, int style, RGB rgb) {
 		super(parent, style);
+		this.color = new Color(parent.getDisplay(),rgb);
 		
 		this.addPaintListener(new PaintListener() {
 			public void paintControl(PaintEvent e) {
 				ClockWidget.this.paintControl(e);
 			}
 		});
+		
+		this.addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+			if(color != null && !color.isDisposed())
+			color.dispose();
+			}
+			});
 		
 		new Thread("TickTock") {
 			public void run() {
@@ -45,12 +64,17 @@ public class ClockWidget extends Canvas{
 
 		e.gc.drawArc(e.x, e.y, e.width - 1, e.height - 1, 0, 360);
 		
+		// add second hand
 		int seconds = new Date().getSeconds();
 		int arc = (15-seconds) * 6 % 360;
-		Color blue = e.display.getSystemColor(SWT.COLOR_BLUE);
-		e.gc.setBackground(blue);
+		e.gc.setBackground(color);
 		e.gc.fillArc(e.x,e.y,e.width-1,e.height-1,arc-1,2);
+		
+		//add hour hand
+		e.gc.setBackground(e.display.getSystemColor(SWT.COLOR_BLACK));
+		int hours = new Date().getHours() + offset;
+		arc = (3 - hours) * 30 % 360;
+		e.gc.fillArc(e.x, e.y, e.width-1, e.height-1, arc - 5, 10);
 	}
-	
 
 }
